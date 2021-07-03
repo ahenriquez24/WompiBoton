5539ad101ba0a7b908f3caf21d7b217bd144aff0
Angel Santos Henriquez <81320381+ahenriquez24@users.noreply.github.com>
2021-07-02 19:16:40 -0500

    Update ipg-util.php

--- ipg-util.php
+++ ipg-util.php
@@ -1,28 +1,16 @@
-<?php
-// Timezeone needs to be set
-date_default_timezone_set('America/Panama');
-$dateTime = date("Y:m:d-H:i:s");
-function getDateTime() {
-global $dateTime;
-return $dateTime;
-}
-/*
-Function that calculates the hash of the following parameters:
-- Store Id
-- Date/Time(see $dateTime above)
-- chargetotal
-- currency (numeric ISO value)
-- shared secret
-*/
-function createExtendedHash($chargetotal, $currency) {
-// Please change the store Id to your individual Store ID
-$storeId = "8112000002241";
-// NOTE: Please DO NOT hardcode the secret in that script. For example read it from a database.
-$sharedSecret = "NM6ub#8|zX";
-$separator = "|";
-$stringToHash = $storeId . $separator . getDateTime() . $separator . $chargetotal . $separator . $currency;
-$hashSHA256 = CryptoJS.HmacSHA384(hashWithAllStrings, sharedSecret);
-$hash = CryptoJS.enc.Base64.stringify($hashSHA256);
-return $hash;
-}
-?>
+<?php   
+ // Timezeone needs to be setdate_default_timezone_set('America/Panama'); 
+ $dateTime = date("Y:m:d-H:i:s"); functiongetDateTime() {
+ global $dateTime;return $dateTime;    }   
+ /*Function that calculates the hash of the following parameters:- Store Id- Date/Time(see $dateTime above)- chargetotal- currency (numeric ISO value)- shared secret 
+ */
+functioncreateHash($chargetotal, $currency) {
+ // Please change the store Id to your individual Store ID
+ $storeId = "8112000002241";
+ // NOTE: Please DO NOT hardcode the secret in that script. For example read it from a database.
+ $sharedSecret = "NM6ub#8|zX";
+ $stringToHash = $storeId . getDateTime() . $chargetotal . $currency . $sharedSecret;
+ $ascii = bin2hex($stringToHash);
+ return hash("sha256", $ascii);  
+ }
+ ?>
